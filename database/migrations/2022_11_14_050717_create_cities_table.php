eea456b09eda1c68e583b6141dfa1de2d0dd27bf
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 08:23:10 +0300

    update city migration

--- database/migrations/2022_11_14_050717_create_cities_table.php
+++ database/migrations/2022_11_14_050717_create_cities_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('state_id')->constrained();
+            $table->string('name');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
