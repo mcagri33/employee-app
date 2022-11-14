c9c41d48feba93c2fb8e00e236bb0297c8dcc131
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 14:04:30 +0300

    add country blades

---
+++ resources/views/countries/edit.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.main')
+@section('title','Country Update')
+@section('castle')
+
+    <main class="page-content">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">@yield('title')</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!--end breadcrumb-->
+
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <div class="card">
+                    <div class="card-header py-3 bg-transparent">
+                        <h5 class="mb-0">@yield('title')</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="border p-3 rounded">
+                            <form class="row g-3" action="{{route('castle.country.update')}}" method="post">
+                                @csrf
+                                <input type="hidden" name="id" value="{{$countryId->id}}">
+                                <div class="col-12">
+                                    <label class="form-label">Country Name</label>
+                                    <input type="text" name="name" value="{{}}" class="form-control" placeholder="Country Name">
+                                    @error("name")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Country Code</label>
+                                    <input type="text" name="country_code" value="{{old('country_code')}}" class="form-control">
+                                    @error("country_code")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary px-4">Update</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--end row-->
+
+    </main>
+
+
+@endsection
