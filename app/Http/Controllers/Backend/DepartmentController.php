27c5e1f1c5d2f6bb5acd45eba3d71f2dd1f1b324
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 20:18:24 +0300

    Added department controller

---
+++ app/Http/Controllers/Backend/DepartmentController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Department;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        $departments = Department::paginate(15);
+        return view('department.index',compact('departments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create()
+    {
+        return view('department.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        Department::create([
+            'name' => $request->name,
+        ]);
+        return redirect()->route('castle.department.index')->with('success', 'Success!');
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function edit($id)
+    {
+        $departmentEdit = Department::find($id);
+        return view('department.edit',compact('departmentEdit'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return RedirectResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $departmentUpdate = Department::findOrFail($request->id);
+        $departmentUpdate->name = $request->name;
+        $departmentUpdate->update();
+        return redirect()->route('castle.department.index')->with('success', 'Success!');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $departmentDelete = Department::findOrFail($id);
+        $departmentDelete->delete();
+        return redirect()->route('castle.department.index')->with('success', 'Success!');
+
+    }
+}
