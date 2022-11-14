4930218ab2688a143dd99541590803b30d6b40c6
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 15:51:33 +0300

    update models

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'middle_name',
+        'address',
+        'department_id',
+        'country_id',
+        'state_id',
+        'city_id',
+        'zip_code',
+        'birthdate',
+        'date_hired'
+    ];
 }
