0fea3ecfee7094721a82c1d2a4c612c26d6ea4fd
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 11:20:51 +0300

    added user blades

---
+++ resources/views/users/add.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.main')
+@section('title','User Add')
+@section('castle')
+
+    <main class="page-content">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">@yield('title')</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!--end breadcrumb-->
+
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <div class="card">
+                    <div class="card-header py-3 bg-transparent">
+                        <h5 class="mb-0">@yield('title')</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="border p-3 rounded">
+                            <form class="row g-3" action="{{route('castle.user.store')}}" method="post">
+                                @csrf
+                                <div class="col-12">
+                                    <label class="form-label">Username</label>
+                                    <input type="text" name="username" value="{{old('username')}}" class="form-control" placeholder="Username">
+                                    @error("username")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">First Name</label>
+                                    <input type="text" name="first_name" value="{{old('first_name')}}" class="form-control" placeholder="First Name">
+                                    @error("first_name")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Last Name</label>
+                                    <input type="text" name="last_name" value="{{old('last_name')}}" class="form-control"
+                                           placeholder="Last Name">
+                                    @error("last_name")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Email</label>
+                                    <input type="email" name="email" value="{{old('email')}}" class="form-control"
+                                           placeholder="example@example.com">
+                                    @error("email")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Password</label>
+                                    <input type="password" name="password"  class="form-control" required autocomplete="new-password">
+                                    @error("password")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="form-label">Password Confirm</label>
+                                    <input type="password" name="password_confirmation" class="form-control" required autocomplete="new-password">
+                                    @error("password_confirmation")
+                                    <span class="text-danger">{{$message}}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary px-4">Add</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--end row-->
+
+    </main>
+
+
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.main')
+@section('title','User List')
+@section('castle')
+
+    <main class="page-content">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">@yield('title')</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="ms-auto">
+                <div class="btn-group">
+                    <a href="{{route('castle.user.add')}}" class="btn btn-primary">User Create</a>
+                </div>
+            </div>
+        </div>
+        <!--end breadcrumb-->
+        @include('layouts.alert')
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <h5 class="mb-0">@yield('title')</h5>
+                    <form class="ms-auto position-relative">
+                        <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i class="bi bi-search"></i></div>
+                        <input class="form-control ps-5" type="text" placeholder="search">
+                    </form>
+                </div>
+                <div class="table-responsive mt-3">
+                    <table class="table align-middle">
+                        @if(count($users)>0)
+                        <thead class="table-secondary">
+                        <tr>
+                            <th>#</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$user->username}}</td>
+                                <td>{{$user->first_name}}</td>
+                                <td>{{$user->last_name}}</td>
+                                <td>{{$user->email}}</td>
+                                <td>
+                                    <div class="table-actions d-flex align-items-center gap-3 fs-6">
+                                        <a href="{{route('castle.user.password',$user->id)}}" class="text-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Change Password"><i class="bi bi-pencil-fill"></i></a>
+                                        <a href="{{route('castle.user.edit',$user->id)}}" class="text-warning" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit"><i class="bi bi-pencil-fill"></i></a>
+                                        <a href="{{route('castle.user.delete',$user->id)}}" class="text-danger" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete"><i class="bi bi-trash-fill"></i></a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                        @else
+                            No found Data
+                        @endif
+                    </table>
+                    <div class="d-flex justify-content-center">
+                        {!! $users->links() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+
+@endsection
