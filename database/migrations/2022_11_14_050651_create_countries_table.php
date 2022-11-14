94f385b49d510a7bc213545ab7a6ad7c2a4d119e
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 08:18:53 +0300

    update country migration

--- database/migrations/2022_11_14_050651_create_countries_table.php
+++ database/migrations/2022_11_14_050651_create_countries_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->char('country_code');
+            $table->string('name');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
