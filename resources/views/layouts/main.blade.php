c1181991c7ce10098528134113ed093d826ad667
cagri <mehmetcagrikaratas@gmail.com>
2022-11-14 10:27:02 +0300

    admin panel template created sections

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en" class="semi-dark">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{asset('assets/images/favicon-32x32.png')}}" type="image/png" />
+    <!--plugins-->
+    <link href="{{asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet"/>
+    <link href="{{asset('assets/plugins/simplebar/css/simplebar.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet" />
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/bootstrap-extended.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/icons.css')}}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
+    <!-- loader-->
+    <link href="{{asset('assets/css/pace.min.css')}}" rel="stylesheet" />
+
+    <!--Theme Styles-->
+    <link href="{{asset('assets/css/dark-theme.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/light-theme.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/semi-dark.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/header-colors.css')}}" rel="stylesheet" />
+
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+
+<!--start wrapper-->
+<div class="wrapper">
+    <!--start top header-->
+   @include('layouts.main.header')
+    <!--end top header-->
+
+    <!--start sidebar -->
+    @include('layouts.main.sidebar')
+
+    <!--end sidebar -->
+
+    <!--start content-->
+   @yield('castle')
+    <!--end page main-->
+
+    <!--start overlay-->
+    <div class="overlay nav-toggle-icon"></div>
+    <!--end overlay-->
+
+    <!--Start Back To Top Button-->
+    <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+    <!--End Back To Top Button-->
+
+</div>
+<!--end wrapper-->
+
+
+<!-- Bootstrap bundle JS -->
+<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+<!--plugins-->
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
+<script src="{{asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
+<script src="{{asset('assets/js/pace.min.js')}}"></script>
+<script src="{{asset('assets/plugins/chartjs/js/Chart.min.js')}}"></script>
+<script src="{{asset('assets/plugins/chartjs/js/Chart.extension.js')}}"></script>
+<script src="{{asset('assets/plugins/apexcharts-bundle/js/apexcharts.min.js')}}"></script>
+<!-- Vector map JavaScript -->
+<script src="{{asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+<script src="{{asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<!--app-->
+<script src="{{asset('assets/js/app.js')}}"></script>
+<script src="{{asset('assets/js/index.js')}}"></script>
+<script>
+    new PerfectScrollbar(".review-list")
+    new PerfectScrollbar(".chat-talk")
+</script>
+
+
+</body>
+
+</html>
---
+++ resources/views/layouts/main/sidebar.blade.php
@@ -0,0 +1,64 @@
+<aside class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="{{asset('assets/images/logo-icon.png')}}" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Snacked</h4>
+        </div>
+        <div class="toggle-icon ms-auto"> <i class="bi bi-list"></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bi bi-house-fill"></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+            <ul>
+                <li> <a href="index.html"><i class="bi bi-circle"></i>Default</a>
+                </li>
+                <li> <a href="index2.html"><i class="bi bi-circle"></i>Alternate</a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="menu-label">Employee Management</li>
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-award-fill"></i>
+                </div>
+                <div class="menu-title">System Management</div>
+            </a>
+            <ul>
+                <li> <a href="#"><i class="bi bi-circle"></i>Country</a>
+                </li>
+                <li> <a href="#"><i class="bi bi-circle"></i>State</a>
+                </li>
+                <li> <a href="#"><i class="bi bi-circle"></i>City</a>
+                </li>
+                <li> <a href="#"><i class="bi bi-circle"></i>Department</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bi bi-award-fill"></i>
+                </div>
+                <div class="menu-title">User Management</div>
+            </a>
+            <ul>
+                <li> <a href="#"><i class="bi bi-circle"></i>User</a>
+                </li>
+                <li> <a href="#"><i class="bi bi-circle"></i>Role</a>
+                </li>
+                <li> <a href="#"><i class="bi bi-circle"></i>Permission</a>
+                </li>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <!--end navigation-->
+</aside>
